de5c232da0dbe59719376ce5d8730edeef730365
Mael7w7 <ange_lito2002@hotmail.com>
2025-11-06 01:11:25 -0500

    bug

--- src/main/java/com/AHC/Portafolio/controller/FormController.java
+++ src/main/java/com/AHC/Portafolio/controller/FormController.java
@@ -3,10 +3,7 @@ package com.AHC.Portafolio.controller;
 import com.AHC.Portafolio.entities.FormEntity;
 import com.AHC.Portafolio.service.FormService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "https://angel-hidalgo-dev.vercel.app") // tu dominio o puerto React
 @RestController
@@ -17,7 +14,7 @@ public class FormController {
     private final FormService formService;
 
     @PostMapping
-    public FormEntity createForm(FormEntity form) {
+    public FormEntity createForm(@RequestBody FormEntity form) {
         return formService.createForm(form);
     }
 }
